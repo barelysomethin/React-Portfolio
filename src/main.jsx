7059cc9a469e174f94160cf426458e048bdbc65e
barelyfayz <132691959+barelysomethin@users.noreply.github.com>
2024-03-14 09:37:28 -0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -8,6 +8,6 @@ ReactDOM.createRoot(document.getElementById('root')).
 render(
   <React.StrictMode>
     <App />
-    <audio  autoPlay src="./src/luellagren Cut.mp3"></audio>
+ 
   </React.StrictMode>,
 )
